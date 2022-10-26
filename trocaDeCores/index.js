4d6f6a9eded87d3f784409723c9ee44c299cacb3
Ana Paula Ribeiro <ribeiroanapaula206@gmail.com>
2022-10-26 17:46:04 -0300

    Add troca de cores no button
    Adicionando evento de troca de cores

--- trocaDeCores/index.js
+++ trocaDeCores/index.js
@@ -7,6 +7,14 @@ window.addEventListener('load',() => {
 function clicandoButton(){
     const buttonTrocaCor = document.getElementById('btn-troca-cor');
     buttonTrocaCor.addEventListener('click', event => {
-        console.log(event);
+        selecionandoCores();
     });
+}
+
+function selecionandoCores (){
+    const elementoBody = document.getElementById('body');
+    const cores = ['#b8af03', '#ee7f27', '#42302e', '#45936c', '#acba9d'];
+    const numeroDeSelecaoDeCor = Math.floor(Math.random() * 4);
+    const cor = cores[numeroDeSelecaoDeCor];
+    elementoBody.style.backgroundColor = cor;
 }
